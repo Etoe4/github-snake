25ecbe07df23b29e0cdfeca7fbdddf929b30a288
Etoe4 <levroetblat@gmail.com>
2025-10-01 19:08:17 +0300

    test3

--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,2 +1,19 @@
-public class Window {
+import java.awt.*;
+import javax.swing.*;
+public class Window extends JPanel  {
+    final int HEIGHT = 1000;
+    final int WIDTH = 1500;
+    JFrame f = new JFrame();
+
+    public Window(){
+    }
+    public void addComponent(Component component) {
+        f.add(component);
+    }
+    public void drawWindow()
+    {
+        f.setSize(WIDTH, HEIGHT);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
 }
